2931c454cddeb6b383e08f14915e17acf3ea2227
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2024-10-06 18:44:17 +0200

    created Client

--- src/main/java/com/akos/client/Client.java
+++ src/main/java/com/akos/client/Client.java
@@ -1,4 +1,34 @@
 package com.akos.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
+    public void startConnection(InetAddress address, int port) {
+        try {
+            clientSocket = new Socket(address, port);
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException ioe) {
+            System.err.println("Error initializing client socket: " + ioe.getMessage());
+        }
+    }
+
+    public void stopConnection() {
+        try {
+            out.close();
+            in.close();
+            clientSocket.close();
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+        }
+    }
+
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
 }
