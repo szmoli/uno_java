6bd5b2e5b4644762d1b2ba414e5f82995421504d
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2024-10-06 23:45:16 +0200

    Player, Deck, Card classes

--- src/main/java/com/akos/uno/game/Deck.java
+++ src/main/java/com/akos/uno/game/Deck.java
@@ -4,5 +4,13 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Deck {
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public void removeCard(Card card) {
+        cards.remove(card);
+    }
+
     List<Card> cards;
 }
--- src/main/java/com/akos/uno/game/Player.java
+++ src/main/java/com/akos/uno/game/Player.java
@@ -1,4 +1,15 @@
 package com.akos.uno.game;
 
+import java.util.List;
+
 public class Player {
+    public void drawCard(Card card) {
+        hand.addCard(card);
+    }
+
+    public void playCard(Card card) {
+        hand.removeCard(card);
+    }
+
+    private Deck hand;
 }
