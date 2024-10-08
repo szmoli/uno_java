df8180d412d1fd699767707a7d53e70a8d5dddca
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2024-10-08 09:50:16 +0200

    added Game constructor

--- src/main/java/com/akos/uno/game/Game.java
+++ src/main/java/com/akos/uno/game/Game.java
@@ -1,8 +1,19 @@
 package com.akos.uno.game;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
+    public Game(GameRules rules) {
+        this.state = GameState.OPEN;
+        this.rules = rules;
+        this.currentPlayerIndex = 0;
+        this.players = new ArrayList<>();
+        this.deck = new Deck();
+        this.currentRound = 1;
+        this.isOrderReversed = false;
+    }
+
     private GameState state;
     private GameRules rules;
     private int currentPlayerIndex;
--- src/main/java/com/akos/uno/game/GameState.java
+++ src/main/java/com/akos/uno/game/GameState.java
@@ -1,5 +1,8 @@
 package com.akos.uno.game;
 
 public enum GameState {
-    WAITING_FOR_PLAYERS, IN_PROGRESS, FINISHED
+    OPEN, // game is open for players to join
+    IN_PROGRESS, // game is in progress, no more players are allowed to join
+    FINISHED, // the game is finished, points are calculated
+    CLOSED // the game was closed after it finished
 }
