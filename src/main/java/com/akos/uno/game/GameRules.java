0b69bf80dd17622596d5dfee571527bf4c7c8070
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2024-10-08 09:43:36 +0200

    added rules to GameRules

--- src/main/java/com/akos/uno/game/GameRules.java
+++ src/main/java/com/akos/uno/game/GameRules.java
@@ -1,4 +1,37 @@
 package com.akos.uno.game;
 
 public class GameRules {
+    public GameRules(boolean isJumpInAllowed, boolean isDrawStackingAllowed, boolean isSevenOAllowed, boolean isWildFourChallengeAllowed, int pointsGoal) {
+        this.isJumpInAllowed = isJumpInAllowed;
+        this.isDrawStackingAllowed = isDrawStackingAllowed;
+        this.isSevenOAllowed = isSevenOAllowed;
+        this.isWildFourChallengeAllowed = isWildFourChallengeAllowed;
+        this.pointsGoal = pointsGoal;
+    }
+
+    public boolean isJumpInAllowed() {
+        return isJumpInAllowed;
+    }
+
+    public boolean isDrawStackingAllowed() {
+        return isDrawStackingAllowed;
+    }
+
+    public boolean isSevenOAllowed() {
+        return isSevenOAllowed;
+    }
+
+    public boolean isWildFourChallengeAllowed() {
+        return isWildFourChallengeAllowed;
+    }
+
+    public int getPointsGoal() {
+        return pointsGoal;
+    }
+
+    private boolean isJumpInAllowed;
+    private boolean isDrawStackingAllowed;
+    private boolean isSevenOAllowed;
+    private boolean isWildFourChallengeAllowed;
+    private int pointsGoal;
 }
