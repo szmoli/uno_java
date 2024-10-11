a4f054cbef02c07c31ba24bf6cec5920d6873959
Szmoleniczki Ákos <145291965+szmoli@users.noreply.github.com>
2024-10-11 11:22:19 +0200

    added json parsing to GameAction

--- src/main/java/com/akos/uno/communication/GameAction.java
+++ src/main/java/com/akos/uno/communication/GameAction.java
@@ -1,10 +1,14 @@
 package com.akos.uno.communication;
 
-import com.akos.uno.game.Player;
+import com.google.gson.Gson;
 
 public abstract class GameAction {
     public String getPlayerName() {
         return playerName;
+    }
+
+    public Gson getGson() {
+        return gson;
     }
 
     public GameAction(String playerName, GameActionType type) {
@@ -16,6 +20,11 @@ public abstract class GameAction {
         return type;
     }
 
+    public String getActionMessage() {
+        return gson.toJson(this);
+    }
+
     private String playerName;
     private GameActionType type;
+    private static final Gson gson = new Gson();
 }
